4d9aa4928607b827a05191f5582febe2a4fa4752
Ivo <ivo.i.karaneshev@gmail.com>
2024-03-14 22:40:23 +0200

    feat: setup migrations

---
+++ database/migrations/2024_03_14_191125_create_contacts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('group_id')->references('id')->on('contacts_groups');
+            $table->string('name')->required();
+            $table->string('nickname')->required();
+            $table->string('phone')->unique()->required();
+            $table->string('email')->unique()->required();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
